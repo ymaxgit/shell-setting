9fcd9b7a16890cfa7f56a285e35fcd2d5fc59684
David Terei <code@davidterei.com>
2015-11-05 15:29:27 -0800

    Fix github add public key script.

--- add-github-keys.sh
+++ add-github-keys.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-curl https://api.github.com/users/dterei/keys | grep key | sed -e 's/\s+"key": "//' | sed -e 's/"$//' >> ~/.ssh/authorized_keys
+curl https://api.github.com/users/dterei/keys \
+  | grep key \
+  | sed -e 's/^\s*"key": "//' \
+  | sed -e 's/"$//' >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
