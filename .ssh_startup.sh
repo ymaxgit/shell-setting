d6439cb4e1886dffe83e2f62f203c2e5cfc1d2df
David Terei <code@davidterei.com>
2015-12-10 14:44:15 -0800

    Update readme on ssh_startup

--- .ssh_startup.sh
+++ .ssh_startup.sh
@@ -1,11 +1,15 @@
-# This script nicley starts up the ssh-agent and loads any private
-# keys of the user. It can handles nicley being called when ssh-agent
-# is already running
+# This script launches a single, persistent ssh-agent process. If one is
+# already running, then it simply sets up the current environment correctly to
+# use it.
 #
-# Author: David Terei
-# Last Modified: 11/08/2008
+# We don't call ssh-add at all as then you are prompted on login to decrypt
+# your ssh-keys, which you may never use. Instead, it's better to wrap the ssh
+# comand with a check to see if any keys are loaded (`ssh-add -l`) and if not,
+# call `ssh-add`. This gives you 'lazy' private key unlocking, which is also
+# slightly better for security.
+#
+# This script provides a similar (but reduced) feature set to keychain.
 
-#ssh-agent
 SSH_ENV=$HOME/.ssh/environment
 TIMEOUT=3600
 
